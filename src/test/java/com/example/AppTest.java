254c71b3921a78e3ad8c19c672b95ea14f52b05c
Userphenixe <hamabbas910@gmail.com>
2026-01-06 12:55:37 +0100

    test: validate console output message

--- src/test/java/com/example/AppTest.java
+++ src/test/java/com/example/AppTest.java
@@ -4,12 +4,30 @@
 package com.example;
 
 import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class AppTest {
+
     @Test
-    void simpleTest() {
-        assertTrue(true);
+    void shouldPrintExpectedMessage() {
+        PrintStream originalOut = System.out;
+
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(out));
+
+        try {
+            App.main(new String[]{});
+        } finally {
+            System.setOut(originalOut);
+        }
+
+        String printed = out.toString();
+        assertTrue(printed.contains("Bonjour et bon courage dans votre projet en DevOps"));
     }
 }
+
 
